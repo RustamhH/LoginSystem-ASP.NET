944ee3e8141f299439ec275e0a8e6085fb820e61
Rustam Hasanli <rustamh2006@gmail.com>
2024-04-29 13:13:37 +0400

    GetAllDesign

--- Login System/obj/Debug/net8.0/Login System.AssemblyInfo.cs
+++ Login System/obj/Debug/net8.0/Login System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Login System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a50c7fff7b64ba6805b59be893bbd27ef5f48c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Login System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Login System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
